00aa02765cef72e0e8bc342390e2c3d178d7b71b
QingXia-Ela <Elahere@163.com>
2022-09-13 21:01:49 +0800

    feat!: 增加无动画自适应

--- js/canvasClass.js
+++ js/canvasClass.js
@@ -50,6 +50,7 @@ class Point {
   update(ParticlePolymerizeFlag = true, options) {
     // 解构变量
     const { Thickness, Drag, Ease, effectParticleMode } = options
+    this.options = options
 
     //移动速度
     this.spx = (this.nx - this.x) / (ParticlePolymerizeFlag ? 30 : 60)
@@ -114,12 +115,14 @@ class Point {
 
   render() {
     const ctx = this.canvas.getContext('2d')
-    ctx.beginPath();
+    let { spacing } = this.options, proportion = window.innerHeight / window.outerHeight
+    spacing *= proportion > 0.5 ? proportion : 0.5
+    ctx.beginPath()
     // 改变初始位置
-    ctx.arc(this.x, this.y, this.size, 0, 360);
-    ctx.fillStyle = `rgba(${this.color},${this.opacity})`;
-    ctx.fill();
-    ctx.closePath();
+    ctx.arc(this.x * spacing, this.y * spacing, this.size, 0, 360)
+    ctx.fillStyle = `rgba(${this.color},${this.opacity})`
+    ctx.fill()
+    ctx.closePath()
   }
 }
 
@@ -129,7 +132,9 @@ class DameDaneParticle {
    * @param {ParticleOptions} options 
    * @param {Function} callback 
    */
-  constructor(canvas, options = {}, callback) {
+  constructor(canvas, options = {
+    spacing: 1
+  }, callback) {
     // 解构
     const { src } = options
 
@@ -164,10 +169,13 @@ class DameDaneParticle {
     // options 备份
     this.options = options
 
+    /** 最终间距，基于窗口高度计算 */
+    this._finalSpacing = this.options.spacing * (window.innerHeight / window.outerHeight)
+
     // 图片加载完成
     this.IMG.onload = () => {
       // 设置解构
-      const { renderX, renderY, w, h, spacing } = this.options
+      const { renderX, renderY, w, h } = this.options
 
       // 渲染起始位置
       this.renderX = renderX
@@ -185,28 +193,38 @@ class DameDaneParticle {
 
       const eleCtx = ele.getContext('2d')
       eleCtx.drawImage(this.IMG, 0, 0, this.ImgW, this.ImgH);
-      const imgArr = eleCtx.getImageData(0, 0, this.ImgW, this.ImgH).data;
+      this._imgArr = eleCtx.getImageData(0, 0, this.ImgW, this.ImgH).data;
       eleCtx.clearRect(0, 0, canvas.width, canvas.height);
-      this._InitParticle(imgArr, spacing)
+      this._InitParticle(this._imgArr, true)
       this._Draw2Canvas()
 
-      // 鼠标事件
-      // this.canvasEle.addEventListener("mousemove", (e) => {
-      //   const cRect = canvas.getBoundingClientRect();
-      //   mx = e.clientX - cRect.left;
-      //   my = e.clientY - cRect.top;
-      // })
       this.hasInit = true
       callback && callback()
     }
+
+    // 鼠标事件
+    this.canvasEle.addEventListener("mousemove", (e) => {
+      const cRect = canvas.getBoundingClientRect();
+      mx = e.clientX - cRect.left;
+      my = e.clientY - cRect.top;
+    })
+
+    // 窗口自适应
+    window.addEventListener('resize', () => {
+      canvas = this.canvasEle
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+    })
   }
 
   /**
    * 图片初始化函数，**此项为内置 api， 不建议随便调用**
    * @param {Uint8ClampedArray} ImgData 图片数据数组
-   * @param {number} Spacing 每个点的间距
+   * @param {boolean} rebuildParticle 是否重组图像
    */
-  _InitParticle = (ImgData, Spacing = 1) => {
+  _InitParticle = (ImgData, rebuildParticle = false) => {
+    if (!ImgData) ImgData = this._imgArr
+
     let imgW = this.ImgW, imgH = this.ImgH, cnt = 0
 
     let arr = this.PointArr
@@ -224,12 +242,12 @@ class DameDaneParticle {
         if (val < 50) {
           if (arr[cnt]) {
             const point = arr[cnt]
-            point.orx = point.nx = w * Spacing + this.renderX
-            point.ory = point.ny = h * Spacing + this.renderY
+            point.orx = point.nx = w + this.renderX
+            point.ory = point.ny = h + this.renderY
             const c = Math.floor(val / 3)
             point.color = `${255 - c},${255 - c},${255 - c}`
           }
-          else arr[cnt] = new Point(w * Spacing + this.renderX, h * Spacing + this.renderY, 1, val, this.canvasEle, this.hasInit)
+          else arr[cnt] = new Point(w + this.renderX, h + this.renderY, 1, val, this.canvasEle, this.hasInit)
           cnt++
         }
       }
@@ -239,18 +257,21 @@ class DameDaneParticle {
       this.PointArr = arr.splice(0, cnt)
 
     // 最终位置打乱
-    arr = this.PointArr
-    let len = arr.length, randIndex = 0, tx = 0, ty = 0
-    while (len) {
-      randIndex = (Math.floor(Math.random() * len--))
-      tx = arr[randIndex].orx, ty = arr[randIndex].ory
+    if (rebuildParticle) {
+      arr = this.PointArr
+      let len = arr.length, randIndex = 0, tx = 0, ty = 0
+      while (len) {
+        randIndex = (Math.floor(Math.random() * len--))
+        tx = arr[randIndex].orx, ty = arr[randIndex].ory
 
-      arr[randIndex].orx = arr[randIndex].nx = arr[len].orx,
-        arr[randIndex].ory = arr[randIndex].ny = arr[len].ory
+        arr[randIndex].orx = arr[randIndex].nx = arr[len].orx,
+          arr[randIndex].ory = arr[randIndex].ny = arr[len].ory
 
-      arr[len].orx = arr[len].nx = tx,
-        arr[len].ory = arr[len].ny = ty
+        arr[len].orx = arr[len].nx = tx,
+          arr[len].ory = arr[len].ny = ty
+      }
     }
+
 
     // 解决散开后切换图片再聚合时粒子没有从随机位置回到正常位置的问题
     if (!this.ParticlePolymerizeFlag) this.ParticlePolymerize(false)
@@ -306,6 +327,6 @@ let f = false
 
 setInterval(() => {
   f ? DameDaneParticleDemo.ChangeImg('./image/test.png', { renderX: 300, w: 360 }) :
-    DameDaneParticleDemo.ChangeImg('./image/island.png', { renderX: 200, renderY: 100, w: 200 })
+    DameDaneParticleDemo.ChangeImg('./image/island.png', { renderX: 500, renderY: 100, w: 300 })
   f = !f
 }, 5000);
